28096c6c26dadb7af24f1d54d01825f717ef6718
Devang-Chauhan-8195 <chauhandevang8195@gmail.com>
2025-02-20 06:51:03 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as new version 
+
+// alert( 3+3 ) //we are using nodejs ,not browser
+
+console.log(3+3)
+
+console.log("Devang")
+
+let name = "Devang"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true /false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+//onject
+
+console.log(typeof "Devang") //string
+console.log(typeof age) //number
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
